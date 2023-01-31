ffa6efd1c2ccb236b3473ef5819e9973b7b039dd
amira-ahmed2 <git>
2023-01-31 16:15:20 +0200

    edit

--- script/home.js
+++ script/home.js
@@ -20,10 +20,10 @@ function showSlides(n) {
     slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active2", "");
   }
   slides[slideIndex-1].style.display = "flex";  
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += " active2";
 }
 // //////////////////
 var coll = document.getElementsByClassName("collapsible");
--- slider/script.js
+++ slider/script.js
@@ -12,3 +12,37 @@ myCarousel.forEach((el) => {
     next = next.nextElementSibling
   }
 })
+
+let myCarousel2 = document.querySelectorAll('#featureContainer2 .carousel .carousel-item');
+myCarousel2.forEach((el) => {
+  const minPerSlide2 = 3
+  let next2 = el.nextElementSibling
+  for (var i=1; i<minPerSlide2; i++) {
+    if (!next2) {
+      // wrap carousel by using first child
+      next2 = myCarousel2[0]
+    }
+    let cloneChild2 = next2.cloneNode(true)
+    el.appendChild(cloneChild2.children[0])
+    next2 = next2.nextElementSibling
+  }
+})
+
+
+
+let myCarousel3 = document.querySelectorAll('#featureContainer3 .carousel .carousel-item');
+myCarousel3.forEach((el) => {
+  const minPerSlide3 = 3
+  let next3 = el.nextElementSibling
+  for (var i=1; i<minPerSlide3; i++) {
+    if (!next3) {
+      // wrap carousel by using first child
+      next3 = myCarousel3[0]
+      
+
+    }
+    let cloneChild3 = next3.cloneNode(true)
+    el.appendChild(cloneChild3.children[0])
+    next3= next3.nextElementSibling
+  }
+});
